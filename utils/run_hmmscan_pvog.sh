64399fe6825e2c11a75c745be7937137f65fc20b
Aaron Pfennig <apfennig3@gatech.edu>
2022-06-17 13:20:46 -0400

    removed hard coding of paths

--- utils/run_hmmscan_pvog.sh
+++ utils/run_hmmscan_pvog.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-while getopts i:o:t: flag
+while getopts i:o:d:t:c: flag
 do
     case "${flag}" in
         i) input=${OPTARG};;
         o) output=${OPTARG};;
+        d) database=${OPTARG};;
         t) table_output=${OPTARG};;
+        c) cpus=${OPTARG};;
     esac
 done
 
-database=/storage3/w/aaron/pvogs_hmm/AllvogHMMprofiles/all_vogs.hmm
 input_centroids=$( echo ${input} | sed 's/.faa/_centroids.faa/' )
 
 python utils/adjust_id.py ${input}
@@ -20,5 +21,5 @@ usearch11.0.667_i86linux32 --cluster_fast ${input}1 -id 0.9 -centroids ${input_c
 
 rm ${input}1
 
-hmmscan -o ${output} --tblout ${table_output} --max --cpu 8 ${database} ${input_centroids}
+hmmscan -o ${output} --tblout ${table_output} --max --cpu ${cpus} ${database} ${input_centroids}
 
--- utils/run_virfinder.R
+++ utils/run_virfinder.R
@@ -1,12 +1,28 @@
+#!/usr/bin/env Rscript
+
 install.packages("glmnet", dependencies=TRUE)
 install.packages("Rcpp", dependencies=TRUE)
+install.packages("optparse", dependencies=TRUE)
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(version = "3.14")
+BiocManager::install()
 BiocManager::install("qvalue")
-install.packages("~/Downloads/VirFinder_1.1.tar.gz", type="source", repos=NULL)
+
+library("optparse")
+option_list = list(
+  make_option(c("--virfinder_tar"), type="character", default="~/Downloads/VirFinder_1.1.tar.gz",
+              help="path to VirFinder tar, which is used to install the package [default= %default]", metavar="character"),
+  make_option(c("-i", "--input"), type="character",
+              help="input file name", metavar="character"),
+  make_option(c("-o", "--output"), type="character",
+              help="output file name", metavar="character")
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+install.packages(opt$virfinder_tar, type="source", repos=NULL)
 
 library(VirFinder)
-predResult <- VF.pred("/home/admin-aaron/genetic_code/all_human_gut_contigs.fna")
+predResult <- VF.pred(opt$input)
 predResult[order(predResult$pvalue),]
-write.table(predResult, file='/home/admin-aaron/genetic_code/VirFinder_human_gut_contigs.tab', quote=FALSE, sep='\t', col.names = TRUE, row.names=TRUE)
+write.table(predResult, file=opt$output, quote=FALSE, sep='\t', col.names = TRUE, row.names=TRUE)
